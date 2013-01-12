b6269b333e86bf21f67e037bfc81a8416727a270
woddfellow2 <woddfellow2@gmail.com>
2013-01-12 04:01:07 -0800

    dwm config.h no longer includes banish.c, as I now use banish (see that repository).

--- .dwm/banish.c
+++
@@ -1,6 +0,0 @@
-void banish()
-{
-	XWarpPointer(dpy, None, RootWindow(dpy, DefaultScreen(dpy)),
-		0, 0, 0, 0, XDisplayWidth(dpy, DefaultScreen(dpy)),
-		XDisplayHeight(dpy, DefaultScreen(dpy)));
-}
